b50da0930a84899c8164d181ddc6c3b284e9fcea
muuuza <muzaankhbayr@gmail.com>
2024-10-08 14:51:50 +0800

    yuuch higegve

--- controller/join.js
+++ controller/join.js
@@ -4,12 +4,10 @@ const { sql } = require("../database");
 exports.join = async (request, response) => {
   const { description, amount, category_image } = request.body;
   try {
-    const join =
-      await sql`SELECT records.description, records.amount, category.category_image, users.username
-  FROM records
-  INNER JOIN category ON records.categoryid=category.categoryid
-  INNER JOIN users ON records.userid=users.userid`;
-    response.status(200).json({ data: join });
+    const join = await sql`SELECT *
+                           FROM Category
+                           INNER JOIN records ON records.categoryid=category.categoryid;`;
+    response.status(200).json(join);
   } catch (error) {
     response.status(400).json(error);
   }
