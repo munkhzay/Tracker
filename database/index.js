8ffcb78465024f00ad07db7fe14ebb0d8e5d53dd
muuuza <muzaankhbayr@gmail.com>
2024-12-25 12:08:18 +0800

    env zsw

--- database/index.js
+++ database/index.js
@@ -1,39 +1,39 @@
-// const postgres = require("postgres");
-// // import postgres from "postgres";
-// require("dotenv").config();
+const postgres = require("postgres");
+// import postgres from "postgres";
+require("dotenv").config();
 
-// let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
 
-// export const sql = postgres({
-//   host: PGHOST,
-//   database: PGDATABASE,
-//   username: PGUSER,
-//   password: PGPASSWORD,
-//   port: 5432,
-//   ssl: "require",
-//   connection: {
-//     options: `project=${ENDPOINT_ID}`,
-//   },
-// });
+export const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
 // async function getPgVersion() {
 //   const result = await sql`select version()`;
 //   console.log(result);
 // }
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  port: 5432, // PostgreSQL-ийн стандарт порт
-  ssl: {
-    rejectUnauthorized: false, // SSL тохиргоог идэвхжүүлсэн
-  },
-});
+// const pool = new Pool({
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   user: process.env.PGUSER,
+//   password: process.env.PGPASSWORD,
+//   port: 5432, // PostgreSQL-ийн стандарт порт
+//   ssl: {
+//     rejectUnauthorized: false, // SSL тохиргоог идэвхжүүлсэн
+//   },
+// });
 
-pool
-  .connect()
-  .then(() => console.log("Connected to PostgreSQL"))
-  .catch((err) => console.error("Error connecting to PostgreSQL:", err));
+// pool
+//   .connect()
+//   .then(() => console.log("Connected to PostgreSQL"))
+//   .catch((err) => console.error("Error connecting to PostgreSQL:", err));
